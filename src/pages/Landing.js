e5b84d6bd484e1c5dc726f14e98caf5d9a8f0187
Chao Ma <machaosydney@gmail.com>
2023-04-04 14:47:21 +1000

    setup routes and pages

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -2,6 +2,7 @@ import main from '../assets/images/main.svg';
 import Wrapper from '../assets/wrappers/LandingPage';
 import React from 'react';
 import { Logo } from '../components';
+import { Link } from 'react-router-dom';
 
 const Landing = () => {
 	return (
@@ -20,7 +21,9 @@ const Landing = () => {
 						Consequatur praesentium laboriosam fugit amet sint culpa delectus
 						cum error maiores distinctio impedit, ab iure itaque mollitia?
 					</p>
-					<button className='btn btn-hero'>Login/Register</button>
+					<Link to='/register' className='btn btn-hero'>
+						Login/Register
+					</Link>
 				</div>
 				<img src={main} alt='job hunt' className='img main-img' />
 			</div>
