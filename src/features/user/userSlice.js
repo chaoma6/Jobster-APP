dd30df4f4b189f7aa0fe5b2c39279bf43ff113aa
Chao Ma <machaosydney@gmail.com>
2023-04-05 14:11:57 +1000

    create dashboard

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,33 +1,72 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { toast } from 'react-toastify';
 import customFetch from '../../utils/axios';
+import {
+	addUserToLocalStorage,
+	getUserFromLocalStorage,
+	removeUserFromLocalStorage,
+} from '../../utils/localstorage';
 
 const initialState = {
 	isLoading: false,
-	user: null,
+	user: getUserFromLocalStorage(),
 };
 
 export const registerUser = createAsyncThunk(
 	'user/registerUser',
 	async (user, thunkAPI) => {
-		// console.log(`Register User : ${JSON.stringify(user)}`);
 		try {
-			const resp = await customFetch.post('/auth/testingRegister', user);
-			console.log(resp);
+			const resp = await customFetch.post('/auth/register', user);
+			return resp.data;
 		} catch (error) {
-			toast.error(error.response.data.msg);
-			console.log(error.response);
+			return thunkAPI.rejectWithValue(error.response.data.msg);
 		}
 	}
 );
 
-export const LoginUser = createAsyncThunk('user/registerUser', async (user, thunkAPI) => {
-	console.log(`Login User : ${JSON.stringify(user)}`);
+export const LoginUser = createAsyncThunk('user/loginUser', async (user, thunkAPI) => {
+	try {
+		const resp = await customFetch.post('/auth/login', user);
+		return resp.data;
+	} catch (error) {
+		return thunkAPI.rejectWithValue(error.response.data.msg);
+	}
 });
 
 const userSlice = createSlice({
 	name: 'user',
 	initialState,
+	extraReducers: (builder) => {
+		builder
+			.addCase(registerUser.pending, (state) => {
+				state.isLoading = true;
+			})
+			.addCase(registerUser.fulfilled, (state, { payload }) => {
+				const { user } = payload;
+				state.isLoading = false;
+				state.user = user;
+				addUserToLocalStorage(user);
+				toast.success(`Hello there ${user.name}`);
+			})
+			.addCase(registerUser.rejected, (state, { payload }) => {
+				state.isLoading = false;
+				toast.error(payload);
+			})
+			.addCase(LoginUser.pending, (state) => {
+				state.isLoading = true;
+			})
+			.addCase(LoginUser.fulfilled, (state, { payload }) => {
+				const { user } = payload;
+				state.isLoading = false;
+				state.user = user;
+				addUserToLocalStorage(user);
+				toast.success(`Welcome Back ${user.name}`);
+			})
+			.addCase(LoginUser.rejected, (state, { payload }) => {
+				state.isLoading = false;
+				toast.error(payload);
+			});
+	},
 });
 
 export default userSlice.reducer;
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -5,6 +5,7 @@ import Wrapper from '../assets/wrappers/RegisterPage';
 import { toast } from 'react-toastify';
 import { useDispatch, useSelector } from 'react-redux';
 import { LoginUser, registerUser } from '../features/user/userSlice';
+import { useNavigate } from 'react-router-dom';
 
 // redux toolkit and useNavigate later
 
@@ -20,6 +21,15 @@ const Register = () => {
 
 	const { isLoading, user } = useSelector((store) => store.user);
 	const dispatch = useDispatch();
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		if (user) {
+			setTimeout(() => {
+				navigate('/');
+			}, 2000);
+		}
+	}, [navigate, user]);
 
 	const handleChange = (e) => {
 		const name = e.target.name;
@@ -72,8 +82,8 @@ const Register = () => {
 					name='password'
 					handleChange={handleChange}
 				/>
-				<button type='submit' className='btn btn-block'>
-					submit
+				<button type='submit' className='btn btn-block' disabled={isLoading}>
+					{isLoading ? 'loading...' : 'submit'}
 				</button>
 				<p>
 					{values.isMember ? 'Not a member yet?' : 'Already a member?'}
