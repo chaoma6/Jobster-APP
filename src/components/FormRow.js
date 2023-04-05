9c78bb32dc723f4332e746e9d126d4b4ebf72b24
Chao Ma <machaosydney@gmail.com>
2023-04-05 22:11:46 +1000

    Refactor User Slice

--- src/components/FormRow.js
+++ src/components/FormRow.js
@@ -4,9 +4,10 @@ const FormRow = ({ type, name, value, handleChange, labelText }) => {
 	return (
 		<div className='form-row'>
 			<label htmlFor={name} className='form-label'>
-				{name}
+				{labelText || name}
 			</label>
 			<input
+				id={name}
 				type={type}
 				name={name}
 				value={value}
@@ -16,5 +17,4 @@ const FormRow = ({ type, name, value, handleChange, labelText }) => {
 		</div>
 	);
 };
-
 export default FormRow;
