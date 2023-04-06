5922210d2ee7e5783f46cae87ce6c24a203bfc65
Chao Ma <machaosydney@gmail.com>
2023-04-06 16:29:23 +1000

    added allJobsSlice

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,19 @@
 import BigSidebar from './BigSidebar';
 import FormRow from './FormRow';
+import Job from './Job';
+import JobsContainer from './JobsContainer';
 import Logo from './Logo';
 import Navbar from './Navbar';
+import SearchContainer from './SearchContainer';
 import SmallSidebar from './SmallSidebar';
 
-export { Logo, FormRow, Navbar, BigSidebar, SmallSidebar };
+export {
+	Logo,
+	FormRow,
+	Navbar,
+	BigSidebar,
+	SmallSidebar,
+	JobsContainer,
+	Job,
+	SearchContainer,
+};
